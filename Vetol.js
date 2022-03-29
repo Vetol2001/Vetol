f28cdc8fc5431dd9bf37a3e829bfd9d3c8c4ded9
Vetol2001 <102618258+Vetol2001@users.noreply.github.com>
2022-03-29 20:58:35 +0300

    Create Vetol.js

---
+++ Vetol.js
@@ -0,0 +1,7 @@
+echo "# Vetol" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Vetol2001/Vetol.git
+git push -u origin main
